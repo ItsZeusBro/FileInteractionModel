0d9f51abf705e0c38a69bd250bc7f2ed8474d807
Zeus <zeusofolympus1000@gmail.com>
2022-07-06 00:02:20 -0400

    we can create a javascript string prototype.flow() so that your path string can be instantiated as a File object

--- File/File.js
+++ File/File.js
@@ -1,8 +1,32 @@
-
+import * as fs from "node:fs"
 export class File{
     constructor(file){
+        File.prototype.flow = this.flow;
+        File.prototype.stream = this.stream;
         this.pre;
         this.post;
+        this.fN=file
+        this.fd=this.getFile(file)
+    }
+    getFile(file){
+        console.log(file)
+        return fs.openSync(file, 'w')
+    }
+    checkFile(){
+        if(fs.existsSync(this.fN)){
+            return true
+        }
+    }
+    flow(b, quant, exclsv, p1, p2, pre, post){
+        var is_file = this.checkFile()
+        if(is_file){        
+            fs.writeFileSync(this.fN, "something")
+        }
+        return b;
+    }
+    stream(b, quant, p1, p2, pre, post){
+        b.file.scripts(pre, post)
+        return b;
     }
     scripts(pre, post){
         this.pre = pre;
--- fist.js
+++ fist.js
@@ -1,35 +1,9 @@
-//Everything having to do with the interface goes here
-//which includes, string sanitization
-//file existence checking
-//try catch handlers (user mitigation NOT INTERNAL)
-//error handling
-//post and prescript handling
-//basically anything to do with the flow of the flow
-import {File} from "./File/File.js";
-class Fist{
-    constructor(file){
-        Fist.prototype.flow = this.flow;
-        Fist.prototype.stream = this.flow;
-        Fist.prototype.pre = this.pre;
-        Fist.prototype.pre = this.pre;
-        this.file = new File(file)
-    }
-    flow(b, quant, exclsv, p1, p2, pre, post){
-        //before any file functions get called on b.file.whatever() or before it recieves any data
-        //set the pre and post scripts using b.file.scripts(pre, post)
-        b.file.scripts(pre, post)
 
-        //use a.file.whatever(b) to perform file operations from a to b
-        
-        return b;
-    }
-    stream(b, quant, p1, p2, pre, post){
-        b.file.scripts(pre, post)
-        return b;
-    }
+
+import {File} from "./File/File.js";
+String.prototype.flow = function(some){
+    console.log(this)
+    return new File(this).flow(some)
 }
 
-var a = new Fist('some/file/somewhere')
-var b = new Fist('some/other/file/somewhere')
-var c = new Fist('some/third/file/somewhere')
-a.flow(b, null, null, null, null, ()=>{}, ()=>{}).flow(c, null, null, null, null, ()=>{}, ()=>{})
+'./some'.flow("some/other/path/to/file").flow("some/third/path/to/file")
