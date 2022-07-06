ad1a2a224163a08484b2826fab8651e9c205566d
Zeus <zeusofolympus1000@gmail.com>
2022-07-06 15:58:58 -0400

    fistfile needs a controller and a separate view for sanitizing inputs

--- src/View/FistFile.js
+++ src/View/FistFile.js
@@ -2,45 +2,22 @@
 import {File} from "../Controller/File/File.js"
 import {Flow} from "../Controller/SexyTransactions/Flow/Flow.js";
 import {Stream} from "../Controller/SexyTransactions/Stream/Stream.js";
-import {Script} from "../Controller/SexyTransactions/Script/Script.js";
-import {Clus} from "../Controller/Interactions/Clus/Clus.js";
-import {Position} from "../Controller/Interactions/Pos/Pos.js";
-import {Quantifier} from "../Controller/Interactions/Quan/Quan.js";
+
 
 class FistFile{
-    constructor(sourcepath, po){
-        this.sourcepath=sourcepath;
-        Fist.prototype.flow = this.flow;
-        Fist.prototype.stream = this.stream;
-        this.File = new File(this, path);
-        this.Flow = new Flow(); //Flow() and Stream() contain pure functions, no need for params in constructor
-        this.Stream = new Stream();
-        if (po){
-            //if this is the first in the chain, it should run
-            //a post script immediately if available on this FistFile
-            new Script(po).po(po)
-        }
+    constructor(file, post){
+       this.Fist(file, post)
     }
     
-    flow(b, q, clsvty, pin, pout, pre, post){
-        q = new Quantifier(q);
-        clsvty = new Clus(this.sourcepath, clsvty);
+    flow(b, ifier, ivity, itionIn, itionOut, pre, post){
 
-        pin = new Position(pin);
-        pout = new Position(pout);
-
-        pre = new Script(pre).pre(pre)
-        this.Flow.flow(b, q, clsvty, pin, pout, pre, post)
-        new Script(po).po(po)
-        return b;   //should always return b!!!
     }
 
     stream(b, q, pin, pout, pre, post){
-        this.Stream.stream(b, q, pin, pout, pre, post)
-        return b;   //should always return b!!!
+
     }
     b(){
-        
+
 
     }
     q(){
