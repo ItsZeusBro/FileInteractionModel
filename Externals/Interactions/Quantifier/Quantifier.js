dbd8e80c11efb30eb985254f3def34fd58b64feb
Zeus <zeusofolympus1000@gmail.com>
2022-07-06 03:45:12 -0400

    and thats what it means

--- Externals/Interactions/Quantifier/Quantifier.js
+++ Externals/Interactions/Quantifier/Quantifier.js
@@ -1,5 +1,6 @@
 import { _Quantifier_ } from "../../Internals/_Quantifier_/_Quantifier_";
 export class Quantifier{
+    //this controls the flow of the data going out of the file in the number of bytes, etc.
     constructor(){
         
     }
---
+++ Internals/_Sexy_/_File_.js
@@ -0,0 +1,146 @@
+import {_Flow_} from "../_Flow_/_Flow_.js"
+import {_Stream_} from "../_Stream_/_Stream_.js"
+import {_Script_} from "./_Script_/_Script_.js"
+import * as fs from "node:fs"
+
+//FILE BUSINESS LOGIC
+class _File_{
+    constructor(path, post){
+
+        //These take this _File_ object in order to access the file
+        //for flowing, streaming, scripting, etc.
+        this._flow_ = new _Flow_(this);
+        this._stream_ = new _Stream_(this);
+        this._pre_ = new _Script_(this); 
+        this._post_ = new _Script_(this);
+        this._path_ = path;
+
+        if (post){
+            //if this is the first in the chain, it should run
+            //a post script immediately if available
+            this._post_.post(post)
+        }
+    }
+
+    flow(b, quant, exclsv, p1, p2, pre, post){
+        //run the prescript on the file
+        b._pre_.pre(pre);
+        //flow the data to b
+        this._flow_.flow(b, quant, exclsv, p1, p2);
+        //run the postscript on the remainder of the file
+        b._post_.post(post);
+    }
+    stream(b, quant, p1, p2, pre, post){
+        //run the prescript on the file
+        b._pre_.pre(pre);
+        //stream the data to b
+        this._stream_.stream(b, quant, p1, p2);
+        //run the postscript on the remainder of the file
+        b._post_.post(post);
+    }
+
+    mkdr(...paths){
+        this.comet('FistPrims.mkdr')
+        for (const dir of paths) {
+          try{
+            if(!fs.existsSync(dir)){
+              fs.mkdirSync(dir)
+            }
+          }catch{
+            return false
+          }
+        }
+      }
+  
+      create(...paths){
+        this.comet('FistPrims.create')
+        for (const p of paths) {
+          try{
+            fs.closeSync(fs.openSync(p, 'w'))
+          }catch{
+            return false
+          }
+        }
+      }
+  
+      rename(olP, newP){
+        this.comet('FistPrims.rename')
+        try{
+          fs.renameSync(olP, newP)
+        }catch{
+          return false
+        }
+      }
+  
+      copyAppend(a, b){
+        this.comet('FistPrims.copyAppend')
+        try{
+          if(fs.existsSync(a)&&fs.existsSync(b)){
+            var buff = fs.readFileSync(a)
+            fs.appendFileSync(b, buff)
+          }
+        }catch{
+          return false
+        }
+      }
+  
+      truncate(p, n){
+        this.comet('FistPrims.truncate')
+          try{
+            if(fs.existsSync(p)){
+              fs.truncateSync(p, n)
+            }
+          }catch{
+            return p
+          }
+      }
+  
+      del(p){
+        this.comet('FistPrims.del')
+          try{
+            if(fs.lstatSync(p).isDirectory()){
+              fs.rmSync(p, {recursive:true})
+            }else{
+              if (fs.existsSync(p)){
+                fs.unlinkSync(p)
+              }
+            }
+          }catch{
+            return false
+          }
+      }
+  
+      write(p, data){
+        this.comet('FistPrims.write')
+        if(Buffer.isBuffer(data)){
+            fs.writeSync(fs.openSync(p, 'a'), data)
+        }else{
+            fs.writeSync(fs.openSync(p, 'a'), Buffer.from(data))
+        }
+      }
+  
+      overwrite(p, data){
+        this.comet('FistPrims.overwrite')
+        if(Buffer.isBuffer(data)){
+          if (fs.existsSync(p)){
+              fs.writeSync(fs.openSync(p, 'w'), data)
+          }
+        }else{
+          if(fs.existsSync(p)){
+            fs.writeSync(fs.openSync(p, 'w'), Buffer.from(data))
+          }
+        }
+      }
+  
+      read(p){
+        this.comet('FistPrims.read')
+        if(fs.existsSync(p)){
+          return fs.readFileSync(p)
+        }
+      }
+  
+      isObject(a){
+        this.comet('FistPrims.isObject')
+        return (!!a) && (a.constructor === Object);
+      };
+}
