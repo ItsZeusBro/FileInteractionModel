33e1a33f95cef4466029db9e1a0cd272f7d875f7
Zeus <zeusofolympus1000@gmail.com>
2022-07-06 03:29:24 -0400

    adding low level functions to internal _Clusive_ class

--- Externals/Clusive/Clusive.js
+++ Externals/Clusive/Clusive.js
@@ -1,8 +1,10 @@
+import { _Clusive_ } from "../../Internals/_Clusive_/_Clusive_";
 export class Clus{
     constructor(ivity, n, m){
         this.clusivity;
         this.n=n;
         this.m=m;
+        this._clusive_ = new _Clusive_()
         if (ivity=="in"){
             this.clusivity = ivity;
         }else if (ivity=="ex"){
@@ -25,46 +27,22 @@ export class Clus{
     }
     ex(source_path, n, m){
         if (n, m){
-            this._ex_range(source_path, n, m)
+            this._clusive_._ex_range(source_path, n, m)
         }else if(n){
-            this._ex_n(source_path, n)
+            this._clusive_._ex_n(source_path, n)
         }else if(m){
-            this._ex_m(source_path, m)
+            this._clusive_._ex_m(source_path, m)
         }else{
-            this._ex(source_path)
+            this._clusive_._ex(source_path)
         }
     }
     in(source_path, n, m){
         if (n, m){
-            this._in_range(source_path, n, m)
+            this._clusive_._in_range(source_path, n, m)
         }else if(n){
-            this._in_n(source_path, n)
+            this._clusive_._in_n(source_path, n)
         }else{
-            this._in_m(source_path, m)
+            this._clusive_._in_m(source_path, m)
         }
-    }
-
-    _ex_range(source_path, n, m){
-
-    }
-    _ex_n(source_path, n){
-
-    }
-    _ex_m(source_path, m){
-
-    }
-    _ex(source_path){
-
-    }
-
-
-    in_range(source_path, n, m){
-
-    }
-    in_n(source_path, n){
-
-    }
-    in_m(source_path, m){
-
     }
 }
--- Internals/_Clusive_/_Clusive_.js
+++ Internals/_Clusive_/_Clusive_.js
@@ -1,5 +1,27 @@
 export class _Clusive_{
     constructor(){
-        
+
+    }
+    _ex_range(source_path, n, m){
+
+    }
+    _ex_n(source_path, n){
+
+    }
+    _ex_m(source_path, m){
+
+    }
+    _ex(source_path){
+
+    }
+
+    _in_range(source_path, n, m){
+
+    }
+    _in_n(source_path, n){
+
+    }
+    _in_m(source_path, m){
+
     }
 }
