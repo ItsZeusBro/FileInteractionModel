6a8c4136c06e00345906687a14431aa3f49ef80c
Zeus <zeusofolympus1000@gmail.com>
2022-07-05 20:33:44 -0400

    flow of the flow

---
+++ fist.js
@@ -0,0 +1,28 @@
+//Everything having to do with the interface goes here
+//which includes, string sanitization
+//file existence checking
+//try catch handlers (user mitigation NOT INTERNAL)
+//error handling
+//post and prescript handling
+//basically anything to do with the flow of the flow
+class Fist{
+    constructor(){
+        Fist.prototype.flow = this.flow;
+        Fist.prototype.stream = this.flow;
+        Fist.prototype.pre = this.pre;
+        Fist.prototype.pre = this.pre;
+
+    }
+    flow(){
+        return this;
+    }
+    stream(){
+        return this;
+    }
+    pre(){
+        return this;
+    }
+    post(){
+        return this;
+    }
+}
