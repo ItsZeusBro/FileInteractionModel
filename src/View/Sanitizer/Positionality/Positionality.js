dbb6a9859178c98bf4d13743d4147b4f20d3acd6
Zeus <zeusofolympus1000@gmail.com>
2022-07-07 02:06:45 -0400

    this fn sucks

--- src/View/Sanitizer/Positionality/Positionality.js
+++ src/View/Sanitizer/Positionality/Positionality.js
@@ -1,12 +1,34 @@
 export class Positionality{
     constructor(string){
         //if we don't catch it throw an exception
-        //pattern 
-        // [end || mid || beg] || 
-        // [+ || - || / || *] || 
-        // [ [0-inf] && [ b || mb || gb || tb || %] || 
-        // [0.0-1.0] || 
-        // [ e && [ [ [_] && [0-inf] ] || [0-inf] ] ]
+        
+//pseudo pattern 
+
+//      ( end || mid || beg ) || [
+//             [ 
+//                  ( + || - || / || * ) && 
+//                  [
+
+//                              [
+//                                  (0-inf) && 
+//                                  [
+//                                      (b || mb || gb || tb || %)
+//                                  ]                                       || 
+//
+//                                  (0.0-1.0)                               || 
+//                              
+//                                  ( e ) && 
+//                                  [ 
+//                                       ( (_) && (0-inf) )                 || 
+//                                       (0-inf) 
+//                                  ]
+//                              ]
+//                  ]
+                
+                
+//              ]
+//        ] 
+        
         this.tokens=[
             'end', 'mid', 'beg', 
             
@@ -42,3 +64,14 @@ export class Positionality{
         //5. evaluate compound expression
         //6. translate it to sanitized object
         //7. return sanitized object
+
+            // 'end', 'end-1b', 'end-1mb', 'end-10tb',
+            // 'end/5b', 'end/2mb', 'end/46gb', 'end/50tb',
+            // 'beg', 'beg+2b', 'beg+3gb', 'beg+4tb',
+            // 'beg*5b', 'beg*2mb', 'beg*46gb', 'beg*50tb',
+            // 'mid', 'mid+3b', 'mid+10mb', 'mid+1000gb', 'mid+10tb',
+            // 'mid-5b', 'mid-100mb', 'mid-100gb', 'mid-10tb',
+            // 'mid/5b', 'mid/10mb', 'mid/2gb', 'mid/100tb',
+            // 'mid+0.5', 'end+0.05', 'beg*0.003', 'beg/0.0005','mid*0.0000003', //some of these would be wrong in validation, but not sanitization
+            // 'mid*0.3e9', 'beg+0.4e_10', 'end+4e40', 'end-30e_3', 'mid+1e100',
+            // '1b+end', '2+mid', 'mid+2'
