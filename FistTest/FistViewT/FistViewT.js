a8246c93a4a7f85f7ea47f99a62947788aab6111
Zeus <zeusofolympus1000@gmail.com>
2022-07-06 23:08:55 -0400

    exhaustive specification in the test file

--- FistTest/FistViewT/FistViewT.js
+++ FistTest/FistViewT/FistViewT.js
@@ -30,6 +30,10 @@ export class FistViewT extends Comet{
     quantality(){
 
         this.comet(
+            `Quantality is evaluated right after positionality for an outgoing transaction
+                which means it is only a single value that represents where from
+                the start of positionality one wants to extend in the amount of data sent
+            `
             `Quantality represents the amount of data flowing from file a to file b`
             `FistView.quan(tality) ACCEPTS end, mid, percentages (strings), +-*/ ops \n`, 
             `Example: 1b, 2mb, 14gb, 0.5, 1b, 5% \n`,
@@ -45,6 +49,12 @@ export class FistViewT extends Comet{
     clusivity(){
 
         this.comet(
+            `This is evaluated after positionality and quantality provide a begining and end anchor
+                we can use beg, mid, and end to denote how much of the outgoing buffer should be shared
+                with the original source. This is a colon separated triplet. The first element is the
+                clusivity (in or ex), the second is beg or mid anchor expressions, the third is relative
+                to the first in the way of a value, or can be anchored by end and used with expressions
+            `
             `FistView.clus(ivity) ACCEPTS beg, end, mid, percentage strings, +-*/ ops \n`, 
             `Example: 1b, 2mb, 14gb, etc. (prefixed with in or ex or just in or ex) 
              in:position:offset or in:0.5, or in:1b, or in:end, in:mid, in:beg (for future?), 
@@ -57,6 +67,7 @@ export class FistViewT extends Comet{
     positionality(){
 
         this.comet(
+            `Positionality is evaluated first! It uses the entire file capacity to determine position`
             `FistView.position(ality) ACCEPTS beg, end, mid (strings), percentages, +-*/ ops \n`, 
             `Examples: 1b, 2mb, 14gb, etc. or 0.5, or 100b, or 5% or beg, end, beg+10b, mid-100mb, etc`
             `FistView.position(ality) RETURNS {ality:''}`
@@ -72,11 +83,11 @@ export class FistViewT extends Comet{
             'mid-5b', 'mid-100mb', 'mid-100gb', 'mid-10tb',
             'mid/5b', 'mid/10mb', 'mid/2gb', 'mid/100tb',
             'mid+0.5', 'end+0.05', 'beg*0.003', 'beg/0.0005','mid*0.0000003', //some of these would be wrong in validation, but not sanitization
-            'mid*0.3e+9', 'beg+0.4e-10', 'end+4e+40', 'end-30e-3', 'mid+1e+100',
+            'mid*0.3e+9', 'beg+0.4e-10', 'end+4e+40', 'end-30e-3', 'mid+1e+100'
         ]
 
         var posFalse=[
-            
+
         ]
 
     }
