5fb818b1bbc9c9d42e8253868e14fa6ecde410d2
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-07 23:46:28 -0400

    Update Quantality.js

--- src/View/Sanitizer/Quantality/Quantality.js
+++ src/View/Sanitizer/Quantality/Quantality.js
@@ -1,5 +1,6 @@
 export class Quantality{
     constructor(string){
+        this.tality = new RegExp("(\b(((0\.[0-9]*)|([1-9][0-9]*%)|([1-9][0-9]*)|(([0-9]*\.[0-9]*))|((mid|end|beg) ?\+?\-?\\?\*?)\s?)(e|bin|b|mb|gb|tb|pb|eb)?([1-9]?[0-9]*%?\s?\-?)((bin|b|mb|gb|tb|pb|eb)?)(((\s|\+)?(\s?\+?\s?)))))\b");
 
     }
 
