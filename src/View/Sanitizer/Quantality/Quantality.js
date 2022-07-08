9002fcdcb6b14d479a4c9cc7fe4cd6c193cdc470
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 02:24:23 -0400

    saving

--- src/View/Sanitizer/Quantality/Quantality.js
+++ src/View/Sanitizer/Quantality/Quantality.js
@@ -1,7 +1,9 @@
 export class Quantality{
     constructor(string){
-        this.tality = new RegExp("/ \\b((\\b(((0\\.[0-9]*)|([1-9][0-9]*%)|([1-9][0-9]*)|(([0-9]*\\.[0-9]*))|((mid|end|beg) ?\\+?\\-?\\\?\\*?\\/?)\\s?)(e|nib|bin|b|mb|gb|tb|pb|eb)?([1-9]?[0-9]*%?\\s?\\-?)((bin|nib|b|mb|gb|tb|pb|eb)?)(((\\s|\\+)?(\\s?\\+?\\s?))))))\\b /g");
+        this.tality = new RegExp("\\b((\\b(((0\\.[0-9]*)|([1-9][0-9]*%)|([1-9][0-9]*)|(([0-9]*\\.[0-9]*))|((mid|end|beg) ?\\+?\\-?\\\?\\*?\\/?)\\s?)(e|nib|bin|b|mb|gb|tb|pb|eb)?([1-9]?[0-9]*%?\\s?\\-?)((bin|nib|b|mb|gb|tb|pb|eb)?)(((\\s|\\+)?(\\s?\\+?\\s?))))))\\b", 'g');
     }
     sanitize(){
     }
 }
+
+//(((((mid|beg|end) ?(\+|\-|\b) ?\b)([0-9]*\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\.[0-9]*)) ?-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)? ?\+? ?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)|(([0-9]*\.[0-9]*)e)
