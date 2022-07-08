ecddd8e942bb9642e30dfaf0445b07c8553177bd
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-08 00:02:45 -0400

    Update Sanitizer.js

--- src/View/Sanitizer/Sanitizer.js
+++ src/View/Sanitizer/Sanitizer.js
@@ -1,11 +1,17 @@
+import {Clusivity} from "./Clusivity/Clusivity.js"
+import {Quantality} from "./Quantality/Quantality.js"
+import {Positionality} from "./Positionality/Positionality.js"
+
 export class Sanitizer{
-    constructor(string, context){
+
+    sanitize(string){
         if (context == 'positionality'){
-
+            var ality = new Positionality();
+            
         }else if (context == 'quantality'){
-
+            var tality = new Quantality();
         }else if (context == 'clusivity'){
-
+            var ivity = new Clusivity();
         }else{
             throw Error("invalid context for sanitizer")
         }
