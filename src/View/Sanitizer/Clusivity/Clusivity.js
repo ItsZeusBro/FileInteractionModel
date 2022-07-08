71ee83ccfef79a0f06f68bc4f0b8026b3ccfd57a
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-08 00:06:38 -0400

    Update Clusivity.js

--- src/View/Sanitizer/Clusivity/Clusivity.js
+++ src/View/Sanitizer/Clusivity/Clusivity.js
@@ -7,15 +7,4 @@ export class Clusivity{
     }
 
 }
-        //2. search for longest keywords first (remember position in string),
-        //3. collect its requirements (evaluate) from base string 
-        //4. insert in value ordered array of objects (repeat)
-        //5. evaluate compound expression
-        //6. translate it to sanitized object
-        //7. return sanitized object
 
-        // `FistView.clus(ivity) ACCEPTS beg, end, mid, percentage strings, +-*/ ops \n`, 
-        //     `Example: 1b, 2mb, 14gb, etc. (prefixed with in or ex or just in or ex) 
-        //      in:position:offset or in:0.5, or in:1b, or in:end, in:mid, in:beg (for future?), 
-        //      ex:5% \n`,
-        //     `FistView.clus(ivity) RETURNS {ivity:'', n:'', m:''}`
