fe1f86cde434fa98ee8720e8b032e97f635482e0
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-08 15:44:02 -0400

    Update Quantality.js

--- src/View/Sanitizer/Quantality/Quantality.js
+++ src/View/Sanitizer/Quantality/Quantality.js
@@ -1,11 +1,11 @@
 export class Quantality{
     constructor(string){
         //CHECK EVERY MATCH FOR ONE THAT WORKS ON THE COMPLETE STRING (ALL OR NOTHING)
-        this.tality = new RegExp("([0-9]*\\.[0-9]*e[0-9]*\\-?([0-9]*)?(bin|b|mb|gb|tb|pb|eb)\\+?[0-9]*(bin|b|mb|gb|tb|pb|eb))|(((((mid|beg|end)(\\+|\\-|\\*|\\\|\\/))([0-9]*\\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\\.[0-9]*))-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]*(bin|b|mb|gb|tb|pb|eb)?\\+?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)|([0-9]*\\.?([0-9]*)?)%?", 'g');
+        this.tality = new RegExp("((((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|nib|b|mb|gb|tb|pb|eb))((\*|\\|\-|\+)(((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|b|mb|gb|tb|pb|eb)))?)*", 'g');
     }
     sanitize(){
     }
 }
 
 
-//((((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|nib|b|mb|gb|tb|pb|eb))((\*|\\|\-|\+)(((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|b|mb|gb|tb|pb|eb)))?)*
+//
