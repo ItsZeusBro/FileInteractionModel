6105bd7943f27cc21ab83e081709e69bb3dd3ef0
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 15:46:41 -0400

    clusivity should work because its the same as quantality except for a prefix

--- src/View/Sanitizer/Clusivity/Clusivity.js
+++ src/View/Sanitizer/Clusivity/Clusivity.js
@@ -1,7 +1,7 @@
 export class Clusivity{
     constructor(string){
         //if we don't catch it throw an exception
-        this.ivity = new RegExp("((in|ex):((end|mid|beg)|(([0-9][0-9]*)))((\\+|\\-|\\/|\\*|\\\|bin|nib|mb|gb|tb|eb|%|\\.)?)(([0-9][0-9]*)?)((bin|nib|mb|gb|tb|eb|%)?)((e(([1-9][0-9]*(\\+|\\-|\\/|\\\|\\*)?([1-9][0-9]*)?)|((((\\+|\\-|\\/|\\\|\\*)[1-9][0-9]*)?)(\\+|\\-|\\/|\\\|\\*)?([1-9][0-9]*)?)|()))?))", 'g');
+        this.ivity = new RegExp("((in|ex):((((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|nib|b|mb|gb|tb|pb|eb))((\*|\\|\-|\+)(((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|b|mb|gb|tb|pb|eb)))?)*)", 'g');
     }
 
 }
