bac8ddf3ac5432f9064950dee0eb25686310d796
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 17:50:19 -0400

    weird

--- Test/Test.js
+++ Test/Test.js
@@ -1,13 +1,13 @@
-import { FistViewT } from "./View/View.js";
+import { View } from "./View/View.js";
 import { Comet } from "../Comet/Comet.js"
 
-class FistTest extends Comet{
+class Test extends Comet{
     constructor(){
         super();
 
     }
     run_tests(){
-        new FistViewT()
+        new View()
     }   
 }
 
