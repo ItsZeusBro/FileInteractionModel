4c42f59051a6c11bea53cd4d77ccbfdeae45ace7
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-08 00:15:07 -0400

    Update Clusivity.js

--- src/View/Sanitizer/Clusivity/Clusivity.js
+++ src/View/Sanitizer/Clusivity/Clusivity.js
@@ -1,7 +1,7 @@
 export class Clusivity{
     constructor(string){
         //if we don't catch it throw an exception
-        this.ivity = new RegExp("(in|ex):((end|mid|beg)|(([0-9][0-9]*)))((\+|-|\/|\*|bin|nib|mb|gb|tb|eb|%|\.)?)(([0-9][0-9]*)?)((bin|nib|mb|gb|tb|eb|%)?)((e(([1-9][0-9]*(\+|-)?([1-9][0-9]*)?)|((((-|\+)[1-9][0-9]*)?)(\+|-)?([1-9][0-9]*)?)|()))?)");
+        this.ivity = new RegExp("(in|ex):((end|mid|beg)|(([0-9][0-9]*)))((\+|-|\/|\*|bin|nib|mb|gb|tb|eb|%|\.)?)(([0-9][0-9]*)?)((bin|nib|mb|gb|tb|eb|%)?)((e(([1-9][0-9]*(\+|-)?([1-9][0-9]*)?)|((((-|\+)[1-9][0-9]*)?)(\+|-)?([1-9][0-9]*)?)|()))?)", "g");
 
 
     }
