ee75faaf997dcd5cea8784c4b39a69d2ba785f55
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 04:43:12 -0400

    do negative test cases tommorow

--- FistTest/FistViewT/FistViewT.js
+++ FistTest/FistViewT/FistViewT.js
@@ -126,7 +126,7 @@ export class FistViewT extends Comet{
             'mid/5b', 'mid/10mb', 'mid/2gb', 'mid/100tb',
             'mid+0.5', 'end+0.05', 'beg*0.003', 'beg/0.0005','mid*0.0000003', //some of these would be wrong in validation, but not sanitization
             'mid*0.3e9', 'beg+0.4e_10', 'end+4e40', 'end-30e_3', 'mid+1e100',
-            '1b+end', '2+mid', 'mid+2'
+            'mid+2'
         ]
 
         var sanitizer = new Sanitizer()
@@ -141,7 +141,7 @@ export class FistViewT extends Comet{
         });
 
         var negatives=[
-
+            '1b+end', '2+mid',
         ]
 
     } 
