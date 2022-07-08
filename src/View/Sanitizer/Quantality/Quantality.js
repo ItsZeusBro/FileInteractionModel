dee8805c1332c6cfd0c9029f74d804feb98aa816
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 02:49:11 -0400

    i think we got it

--- src/View/Sanitizer/Quantality/Quantality.js
+++ src/View/Sanitizer/Quantality/Quantality.js
@@ -1,14 +1,11 @@
 export class Quantality{
     constructor(string){
+        //CHECK EVERY MATCH FOR ONE THAT WORKS ON THE COMPLETE STRING (ALL OR NOTHING)
         this.tality = new RegExp("\\b((\\b(((0\\.[0-9]*)|([1-9][0-9]*%)|([1-9][0-9]*)|(([0-9]*\\.[0-9]*))|((mid|end|beg) ?\\+?\\-?\\\?\\*?\\/?)\\s?)(e|nib|bin|b|mb|gb|tb|pb|eb)?([1-9]?[0-9]*%?\\s?\\-?)((bin|nib|b|mb|gb|tb|pb|eb)?)(((\\s|\\+)?(\\s?\\+?\\s?))))))\\b", 'g');
     }
     sanitize(){
     }
 }
 
-//(((((mid|beg|end) ?(\+|\-|\b) ?\b)([0-9]*\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\.[0-9]*)) ?-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)? ?\+? ?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)|(([0-9]*\.[0-9]*)e)
-//(((((mid|beg|end) ?(\+|\-|\b) ?\b)([0-9]*\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\.[0-9]*)) ?-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)? ?\+? ?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)|(([0-9]*\.?%?[0-9]*)e?([0-9]*)? ?\-?(bin|b|mb|gb|tb|pb|eb)?(\+|\-)? ?([0-9]*)?(bin|b|mb|gb|tb|pb|eb)?)
-
-
-
-//(((((mid|beg|end) ?(\+|\-|\b) ?\b)([0-9]*\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\.[0-9]*)) ?-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)? ?\+? ?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)| //this one is good, just add the next to it
+//I think we got it
+//(((((mid|beg|end) ?(\+|\-|\b) ?\b)([0-9]*\.[0-9]*)?((e[0-9]*)|[0-9]*)?)|([0-9]*\.[0-9]*)) ?-?%?(bin|b|mb|gb|tb|pb|eb)?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)? ?\+? ?([0-9]*(bin|b|mb|gb|tb|pb|eb))?)|([0-9]*(\.)?[0-9]*((e|%)[0-9]*)? ?\-?([0-9]*)(bin|b|mb|gb|tb|pb|eb)? ?\+? ?[0-9]* ?(bin|b|mb|gb|tb|pb|eb)?)
