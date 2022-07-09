374fb99618b11b39976e2e074f15dcd6e55bd996
zeus <zeusofolympus1000@gmail.com>
2022-07-09 18:22:33 -0400

    back to reality

---
+++ Source/View/Positionality/Positionality.js
@@ -0,0 +1,13 @@
+import { BIT, KBIT, MBIT, GBIT, TBIT, PBIT, EBIT, MID, BEG, END} from "../../GoodyBag/GoodyBag.js";
+import {ANYTHING_SCIENTIFIC} from "../../../MagicSpells/MagicSpells.js"
+export class Positionality{
+    //(type of number)[multiplier][ops]((type of number)[multiplier][ops])*
+    constructor(){
+       
+    }
+    sanatize(){
+        this.scientize(ality))
+    }
+    
+
+}
---
+++ Source/View/Sanatize.js
@@ -0,0 +1,51 @@
+
+export class Sanatize{
+    constructor(){
+        this.binalities = {
+            "bit":BIT,"kbit":KBIT,"mbit":MBIT, "gbit":GBIT, "tbit":TBIT, "pbit":PBIT, "ebit":EBIT,
+            "mid":MID, "beg":BEG, "end":END
+        };
+        this.ality=/(^((\+|\-)?((beg|end|mid)|([0-9]*\.?[0-9]*e\+?\-?[0-9]*)|([0-9]*\.[0-9]*)|(bin|bit|kbit|mbit|gbit|tbit|pbit|ebit)|([0-9]*))%?)((?!\*\*)(\*|\+|\-|\\|\/)?(\+|\-)?(((beg|end|mid)|([0-9]*\.?[0-9]*e\+?\-?[0-9]*)|([0-9]*\.[0-9]*)|(bin|bit|kbit|mbit|gbit|tbit|pbit|ebit)|([0-9]*))%?)?)*)/g;
+        this.tality=/(^((\+|\-)?((beg|end|mid)|([0-9]*\.?[0-9]*e\+?\-?[0-9]*)|([0-9]*\.[0-9]*)|(bin|bit|kbit|mbit|gbit|tbit|pbit|ebit)|([0-9]*))%?)((?!\*\*)(\*|\+|\-|\\|\/)?(\+|\-)?(((beg|end|mid)|([0-9]*\.?[0-9]*e\+?\-?[0-9]*)|([0-9]*\.[0-9]*)|(bin|bit|kbit|mbit|gbit|tbit|pbit|ebit)|([0-9]*))%?)?)*)/g;
+        this.ivity=/ /g;
+    }
+    sanatize(sPosaQuanaClus, context){
+        if (context == 'positionality'){
+            return this._sanatize(this.scientize(this.match(this.ality, sPosaQuanaClus, context)))
+        }else if (context == 'quantality'){
+            return this._sanatize(this.scientize(this.match(this.tality, sPosaQuanaClus, context)))
+        }else if (context == 'clusivity'){
+            return this._sanatize(this.scientize(this.match(this.ivity, sPosaQuanaClus, context)))
+        }else{
+            throw Error("invalid context for sanitizer")
+        }
+    }
+
+    match(reg, sPosaQuanaClus, context){
+        var reg = new RegExp(reg);
+        var match;
+        try{
+            var spacedOut=sPosaQuanaClus.split(' ').join('');
+            match = spacedOut.match(reg)[0];
+        }catch{
+            throw Error(context+" is not properly formatted")
+        }
+        return match;
+    }
+    scientize(sPosaQuanaClus){
+        var spacedOut=sPosaQuanaClus.split(' ').join('');
+        match = this.match(sPosaQuanaClus);
+        var scientized;
+        var reg= new RegExp(ANYTHING_SCIENTIFIC)
+        try{
+            reg.exec(match).forEach(scientality => {
+                if(JSON.stringify(new Number(scientality))){
+                    scientized = match.replace(reg, new Number(scientality))
+                }
+            });
+            return scientized
+        }catch{
+            return sPosaQuanaClus
+        }
+    }
+}
