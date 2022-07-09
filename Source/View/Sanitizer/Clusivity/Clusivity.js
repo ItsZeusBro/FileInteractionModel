f97c4c8d72e4ed52f479ccecb29e85d6fd7aec14
Zeus <zeusofolympus1000@gmail.com>
2022-07-08 20:47:57 -0400

    wouldbivity ivityTok(ity)

--- Source/View/Sanitizer/Clusivity/Clusivity.js
+++ Source/View/Sanitizer/Clusivity/Clusivity.js
@@ -7,17 +7,20 @@ export class Clusivity{
         ]
     }
 
-    isIvity(string){
-        var spacedOut=string.split(' ').join('')
+    isIvity(wouldbivity){
+        var spacedOut=wouldbivity.split(' ').join('')
         var ivityReg = new RegExp(/((in|ex):((((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|nib|b|mb|gb|tb|pb|eb))((\*|\/|\-|\+)(((mid|beg|end)|(((0\.)|([0-9]*\.))?[0-9]*%?(e(([0-9]*)|((\+|\-)[0-9]*)))?))|(bin|b|mb|gb|tb|pb|eb)))?)*)/g);
         return {"matches":spacedOut.match(ivityReg), "spacedout":spacedOut, "original":string}
     }
 
     //(type of number)[multiplier][ops]((type of number)[multiplier][ops])*
-    ivityTok(){
+    ivityTok(ity){
         //gets the next token in the ality string
     }
-    ivityTokenize(){
+    ivityTokenize(wouldbivity){
         //gets all tokens in the ality string
+    }
+    sanitize(wouldbivity){
+        
     }
 }
